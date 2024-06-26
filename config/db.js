f9c30693352ed4e9a350fe0d65ac43c599001d53
Pooja30031998 <85399615+Pooja30031998@users.noreply.github.com>
2024-06-26 16:29:00 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/HabitTracker");
+    await mongoose.connect(process.env.DB_URL);
     console.log("MongoDB connected using mongoose");
   } catch (error) {
     console.log(error);
